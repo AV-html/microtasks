a79f6f2160b264431424a4dc84c3cb68d1099655
Alexander Volkov <AV-html@yandex.ru>
2022-04-30 20:36:52 +0300

    3 - universal button

--- src/3-button/App3.tsx
+++ src/3-button/App3.tsx
@@ -1,54 +1,30 @@
 import React from "react";
+import {Button} from "./components/Button";
 
 
 export const App3 = () => {
-    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hello! I\'m Paul!');
-    // }
-    // const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hello! I\'m Peter!');
-    // }
 
-    const onClickHandler = (name: string) => {
-        console.log(`Hello! I\'m ${name}!`);
+    const ButtonFoo = (subscriber: string, age: number) => {
+        console.log(subscriber, age);
     }
 
-
-    // const foo1 = () => {
-    //     console.log(100200);
-    // }
-    // const foo2 = (num: number) => {
-    //     // передать цифру 100200
-    //     console.log(num);
-    // }
+    const Button3Foo = () => {
+        console.log('I\'m stupid button!')
+    }
 
     return (
         <div>
             <hr/>
-            <h2>3 - button</h2>
+            <h2>3 - universal button</h2>
 
-            {/*<button onClick={(event) => {*/}
-            {/*    console.log('Hello')*/}
-            {/*}}>*/}
-            {/*    MyYouTubeChanel-1*/}
-            {/*</button>*/}
-
-            <button onClick={() => {
-                onClickHandler('Paul')
-            }}>
-                MyYouTubeChanel-1
-            </button>
-            <button onClick={() => {
-                onClickHandler('Peter')
-            }}>
-                MyYouTubeChanel-2
-            </button>
-
-            {/*<button onClick={foo1}>1</button>*/}
-            {/*<button onClick={() => {*/}
-            {/*    foo2(100200)*/}
-            {/*}}>2*/}
-            {/*</button>*/}
+            <Button name={"MyYouTubeChanel-1"} callback={() => {
+                ButtonFoo('Ivan', 21)
+            }}/>
+            <Button name={"MyYouTubeChanel-2"} callback={() => {
+                ButtonFoo('Peter', 23)
+            }}/>
+            <Button name={"Stupid BUTTON"} callback={Button3Foo}/>
+            
             <hr/>
         </div>
     );
